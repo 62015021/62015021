9abec59cbb2173c87c926584d3f00e5834010068
ชญานิน บัวสละ <77890994+62015021@users.noreply.github.com>
2021-10-19 02:07:26 +0700

    Delete book directory

--- book/BookReader.java
+++
@@ -1,9 +0,0 @@
-package solid.book;
-import java.util.List;
-
-public class BookReader {
-    public static void main(String[] args) {
-        ScreenPrint book = new ScreenPrint("Tyland", List.of("I", "moved", "here", "recently", "too"));
-        book.printToScreen();
-    }
-}
